fe6b99b394b92c121004b62f267675e2dd184ac1
Tino Vyatkin <tino@vtkn.io>
2019-08-22 14:23:24 -0400

    20/08

--- src/daily-coding-20-08-2019.kt
+++ src/daily-coding-20-08-2019.kt
@@ -1,5 +1,3 @@
-import java.util.stream.Stream
-import kotlin.streams.asStream
 
 /**
 
@@ -21,18 +19,77 @@ For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should pri
 
  */
 
-fun findMedian(seq: Iterable<Int>) {
-    var m1 = 0; // middle-left number
-    val idx1 = 0;
-    var m2 = 0; // middle-right number
-    val idx2 = 0;
-    val iterator = seq.iterator().withIndex();
-    while(iterator.hasNext()) {
+/*
 
-        println(iterator.next())
+Reasoning:
+
+let's keep array of 4 Ints = [0, 0, 0, 0];
+
+vars:
+
+v - our array of 4 numbers
+idx - current number index in stream
+n - current value
+
+steps:
+
+idx === 0: put n at v[1]
+ids === 1: if(n > v[1]) v[2] = n; else { v.unshift(0); v[1] = n; }
+ids === 2: if(n < v[1]) v[0] = n; else { if(n < v[2]) { v[3] = v[2]; v[2] = n };  else {v[3] = n} }
+ids === 3: v += n; v.sort().slice(0)
+
+next steps
+
+
+return (v[1] + v[2]) / 2
+
+ */
+
+fun findMedian(seq: Iterable<Int>) {
+    val allint = mutableListOf<Int>();
+    var heap = mutableListOf<Int>();
+    val iterator = seq.iterator().withIndex();
+    while (iterator.hasNext()) {
+        val n = iterator.next();
+        allint.add(n.value);
+        when (n.index) {
+            0, 1 -> {
+                heap.add(n.value); println(heap.average())
+            }
+            2, 3 -> {
+                heap.add(n.value);
+                heap.sort();
+                println(heap);
+                // if len is even, then return average of middle
+                // else return just one middle element
+                if ((n.index + 1) % 2 == 0) println(heap.slice(1..2).average())
+                else println(heap[1])
+            }
+            else -> {
+                if (n.value <= heap[0]) {
+                    // put it at start and remove last element
+                    heap.add(0, n.value)
+                    heap.removeAt(4)
+                } else if (n.value >= heap[3]) {
+                    // put it at end and remove first element
+                    heap.add(n.value)
+                    heap.removeAt(0);
+                } else {
+                    // put it into the list, sort, and remove last one
+                    heap.add(n.value);
+                    heap.sort();
+                    heap.removeAt(4);
+                }
+                println("For list: ${ allint.sorted() } (size: ${allint.size}), heap is: ${heap}");
+                if (n.index % 2 == 0) println(heap.slice(1..2).average())
+                else println(heap.slice(2..3).average())
+            }
+
+        }
+
     }
 }
 
 fun main() {
-    findMedian(listOf(2, 1, 5, 7, 2, 0, 5));
+    findMedian(listOf(2, 1, 5, 7, 2, 0, 5, -1, 10, 0, 5, 4, 4, 4, 4));
 }
